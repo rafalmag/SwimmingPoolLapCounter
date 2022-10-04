6b56bae27605c13325596f23f079e5822f1f2200
Rafal Magda <rafalmag@gmail.com>
2022-10-04 21:51:24 +0200

    eink works

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # SwimmingPoolLapCounter
+
+Hardware:
+- Arduino Nano
+- e-ink MH-ET LIVE - see https://github.com/MHEtLive/MH-ET-LIVE-E-Papers 
+
+Wiring:
+
+| e-ink / paper | Nano board |
+|---------------|------------|
+| VCC           | 5V         |
+| GND           | GND        |
+| SDI (DIN)     | D11(MOSI)  |
+| CLK           | D13(SCK)   |
+| CS            | D10        |
+| DC            | D9         |
+| RESET         | D8         |
+| BUSY          | D7         |
+
+![wiring](guide-wiring-eink-epaper-module-schematics-diagram-14core-768x633.jpg "wiring")
--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,119 @@
-#include <Arduino.h>
+/*demo for MH-ET LVIE 1.54Black Epaper*/
+/*
+*connection:
+Board.UNO/2560.etc-------------epaper module
+D7--------------------------------Busy
+D8--------------------------------Reset
+D9--------------------------------D/C
+D10-------------------------------CS
+D11-------------------------------SDI
+D13-------------------------------SCLK
+VCC-------------------------------VCC
+GND------------------------------GND
+*Set the toggle switch to the correct gear position according to the supply voltage.
+*SPI:4line-wire
+*/
+#include <epd1in54.h>
+#include <epdpaint.h>
 
-#include <SPI.h>
-#include <EPD1in54.h>
-#include <EPDPaint.h>
-#include "imagedata.h"
+#define COLORED     1
+#define UNCOLORED   0
 
-// The Nano (like all Arduinos) has an LED attached to digital pin 13, which is also 
-//  used for SPI: 10 (SS), 11 (MOSI), 12 (MISO), 13 (SCK).
-EPD1in54 epd; // default reset: 8, dc: 9, cs: 10, busy: 7
+/**
+  * Due to RAM not enough in Arduino UNO or NANO a pattern buffer is not allowed.
+  * In this case, a smaller image buffer is allocated and you have to 
+  * update a partial display several times.
+  * 1 byte = 8 pixels, therefore you have to set 8*N pixels at a time.
+  */
+unsigned char image[1024];
+Paint paint(image, 0, 0);    // width should be the multiple of 8 
+Epd epd;
+unsigned long time_start_ms;
+unsigned long time_now_s;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  Serial.println("Hello World!");
-  // pinMode(LED_BUILTIN, OUTPUT);
-  // digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-
-  if (epd.init(lutFullUpdate) != 0) {
-    Serial.print("e-Paper init failed");
-    return;
+  if (epd.Init(lut_full_update) != 0) {
+      Serial.print("e-Paper init failed");
+      return;
   }
 
-  epd.setFrameMemory(IMAGE_DATA);
-  epd.displayFrame();
-  epd.setFrameMemory(IMAGE_DATA);
-  epd.displayFrame();
+  /** 
+   *  there are 2 memory areas embedded in the e-paper display
+   *  and once the display is refreshed, the memory area will be auto-toggled,
+   *  i.e. the next action of SetPatternMemory will set the other memory area
+   *  therefore you have to clear the pattern memory twice.
+   */
+  epd.ClearPatternMemory(0xFF);   // bit set = white, bit reset = black
+  epd.DisplayPattern();
+  epd.ClearPatternMemory(0xFF);   // bit set = white, bit reset = black
+  epd.DisplayPattern();
+
+  paint.SetRotate(ROTATE_0);
+  paint.SetWidth(200);
+  paint.SetHeight(24);
+
+  /* For simplicity, the arguments are explicit numerical coordinates */
+  paint.Clear(COLORED);
+  paint.DrawStringAt(30, 4, "Hello world!", &Font16, UNCOLORED);
+  epd.SetPatternMemory(paint.GetImage(), 0, 10, paint.GetWidth(), paint.GetHeight());
+  
+  paint.Clear(UNCOLORED);
+  paint.DrawStringAt(16, 4, "---MH---", &Font24, COLORED);
+  epd.SetPatternMemory(paint.GetImage(), 0, 30, paint.GetWidth(), paint.GetHeight());
+
+  paint.SetWidth(64);
+  paint.SetHeight(64);
+  
+  paint.Clear(UNCOLORED);
+  paint.DrawRectangle(0, 0, 40, 50, COLORED);
+  paint.DrawLine(0, 0, 40, 50, COLORED);
+  paint.DrawLine(40, 0, 0, 50, COLORED);
+  epd.SetPatternMemory(paint.GetImage(), 16, 60, paint.GetWidth(), paint.GetHeight());
+
+  paint.Clear(UNCOLORED);
+  paint.DrawCircle(32, 32, 30, COLORED);
+  epd.SetPatternMemory(paint.GetImage(), 120, 60, paint.GetWidth(), paint.GetHeight());
+
+  paint.Clear(UNCOLORED);
+  paint.DrawFilledRectangle(0, 0, 40, 50, COLORED);
+  epd.SetPatternMemory(paint.GetImage(), 16, 130, paint.GetWidth(), paint.GetHeight());
+
+  paint.Clear(UNCOLORED);
+  paint.DrawFilledCircle(32, 32, 30, COLORED);
+  epd.SetPatternMemory(paint.GetImage(), 120, 130, paint.GetWidth(), paint.GetHeight());
+  epd.DisplayPattern();
+
+  delay(2000);
+
+  if (epd.Init(lut_partial_update) != 0) {
+      Serial.print("e-Paper init failed");
+      return;
+  }
+
+  /** 
+   *  there are 2 memory areas embedded in the e-paper display
+   *  and once the display is refreshed, the memory area will be auto-toggled,
+   *  i.e. the next action of SetPatternMemory will set the other memory area
+   *  therefore you have to set the pattern memory and refresh the display twice.
+   */
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  char time_string[] = {'0', '0', ':', '0', '0', '\0'};
 
+  paint.SetWidth(90);
+  paint.SetHeight(32);
+  paint.SetRotate(ROTATE_0);
+
+  paint.Clear(UNCOLORED);
+  paint.DrawStringAt(0, 4, time_string, &Font24, COLORED);
+  epd.SetPatternMemory(paint.GetImage(), 8, 128, paint.GetWidth(), paint.GetHeight());
+  epd.DisplayPattern();
+
+  delay(500);
 }
+
